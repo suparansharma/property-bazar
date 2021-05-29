2c3954e2bf2f527b959f32f0524eb0b96593ccc2
shihab17 <shihabahmed620@gmail.com>
2021-05-29 22:08:55 +0600

    data access class added

---
+++ property-bazar/Database/DataAccess.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace property_bazar.Database
+{
+    class DataAccess
+    {
+        public string ConnectionString { get; set; }
+        public DataAccess()
+        {
+            ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=F:\AIUB\C#\HospitalMangement\hospital.mdf;Integrated Security=True;Connect Timeout=30";
+        }
+        public SqlCommand GetCommand(string sqlQuery)
+        {
+            SqlConnection conn = new SqlConnection(ConnectionString);
+            SqlCommand sqlCmd = new SqlCommand(sqlQuery, conn);
+            return sqlCmd;
+
+        }
+        public DataTable Execute(string sql)
+        {
+            SqlCommand cmd = GetCommand(sql);
+            DataTable dt = Execute(cmd);
+            return dt;
+        }
+        public DataTable Execute(SqlCommand command)
+        {
+            DataTable dt = new DataTable();
+            command.Connection.Open();
+            dt.Load(command.ExecuteReader());
+            command.Connection.Close();
+            return dt;
+        }
+    }
+}
