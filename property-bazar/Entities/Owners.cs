dff80fda1fcf7d5b8368abdc14c3ec4d53053e7d
Shihab Ahmed Efty <shihabahmed620@gmail.com>
2021-06-01 18:04:39 +0600

    owner class added

---
+++ property-bazar/Entities/Owners.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace property_bazar.Entities
+{
+    class Owners
+    {
+        public int ownerId;
+        public string ownerUserName;
+        public string ownerPassword;
+
+        public Owners(int ownerId, string ownerUserName, string ownerPassword)
+        {
+            this.ownerId = ownerId;
+            this.ownerUserName = ownerUserName;
+            this.ownerPassword = ownerPassword;
+        }
+    }
+}
